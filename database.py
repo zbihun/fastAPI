0529e83d9dda380782bae96955277c0a88d1a974
zbihun <bighun.zoryana@gmail.com>
2025-05-22 21:43:27 +0300

    Adding requirements

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:dolyna@localhost/ToDoAppDB"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+#"postgresql://postgres:dolyna@localhost/ToDoAppDB"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
